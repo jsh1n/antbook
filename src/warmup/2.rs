8aab2b256a531467c1fb2e5fd4755cc4a7fec663
tiuuuuPJ <pj.kyosyou@outlook.jp>
2018-05-26 07:04:13 +0900

    fix 2.rs

--- src/warmup/2.rs
+++ src/warmup/2.rs
@@ -1,7 +1,7 @@
 fn main() {
     // argument
     let l: u8 = 10;
-    let x: Vec<u8> = vec!(2, 6, 7);
+    let x: Vec<u8> = vec![2, 6, 7];
 
     // exec
     println!("(min, max)={:?}", solve(l, x));
@@ -13,13 +13,13 @@ fn solve(l: u8, x: Vec<u8>) -> (u8, u8) {
     //min steps
     let mut min_steps = 0;
     for position in &x {
-        min_steps = cmp::max(cmp::min(*position, l-*position), min_steps);
+        min_steps = cmp::max(cmp::min(*position, l - *position), min_steps);
     }
 
     //max steps
     let mut max_steps = 0;
     for position in &x {
-        max_steps = cmp::max(cmp::max(*position, l-*position), max_steps);
+        max_steps = cmp::max(cmp::max(*position, l - *position), max_steps);
     }
 
     return (min_steps, max_steps);
@@ -27,5 +27,5 @@ fn solve(l: u8, x: Vec<u8>) -> (u8, u8) {
 
 #[test]
 fn answer_check() {
-    assert_eq!(solve(10, vec!(2, 6, 7)), (4, 8));
+    assert_eq!(solve(10, vec![2, 6, 7]), (4, 8));
 }
