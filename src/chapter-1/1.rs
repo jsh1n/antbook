f022655ad3d0a9bfdecf2b3037d9cb425dff4ceb
tiuuuuPJ <pj.kyosyou@outlook.jp>
2018-05-26 16:14:54 +0900

    add binary_search

--- src/chapter-1/1.rs
+++ src/chapter-1/1.rs
@@ -1,45 +1,55 @@
 fn main() {
     // argument
     let n: usize = 3;
-    let m: u32 = 10;
-    let k: Vec<u32> = vec![1, 3, 5];
+    let m: i32 = 9;
+    let k: Vec<i32> = vec![1, 3, 5];
 
     // execution
     println!("{}", solve(n, m, k));
 }
 #[warn(dead_code)]
 
-fn binary_search(mut v: Vec<u32>, search_value: u32) -> bool {
+fn binary_search(slice: &mut [i32], search_value: i32) -> bool {
     let mut l: usize = 0;
-    let mut r: usize = v.len();
+    let mut r: usize = slice.len();
 
-    v.sort();
+    slice.sort();
+    println!("{:?}, {}",slice, search_value);
     while r - l >= 1 {
         let i: usize = (l + r) / 2;
-        if v[i] == search_value {
+        if slice[i] == search_value {
             return true;
-        } else if v[i] < search_value {
+        } else if slice[i] < search_value {
             l = i + 1;
         } else {
             r = i;
         }
+        println!("r: {}, l: {}", r, l);
     }
     return false;
 }
 
-fn solve(n: usize, m: u32, k: Vec<u32>) -> bool {
-    let mut vec: Vec<u32> = Vec::new();
+fn solve(n: usize, m: i32, k: Vec<i32>) -> bool {
+    let mut vec: Vec<i32> = Vec::new();
     for i in 0..k.len() {
         for j in 0..k.len() {
-            vec.push(k[i] * k[j]);
+            vec.push(k[i] + k[j]);
         }
     }
+    let slice = &mut vec[..];
     for a in 0..n {
         for b in 0..n {
-            if binary_search(vec, m - k[a] - k[b]) {
+            println!("{}, {}", k[a], k[b]);
+            if binary_search(slice, m - k[a] - k[b]) {
                 return true;
             }
         }
     }
     return false;
+}
+
+#[test]
+fn check_answer() {
+    assert_eq!(solve(3, 10, vec![1, 3, 5]), true);
+    assert_eq!(solve(3, 9, vec![1, 3, 5]), false);
 }
