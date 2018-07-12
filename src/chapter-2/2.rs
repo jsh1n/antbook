056b206102451ad17a4461940de6035b5b85c206
pj <pj.kyosyou@outlook.jp>
2018-07-13 03:35:38 +0900

    impl count of pond

--- src/chapter-2/2.rs
+++ src/chapter-2/2.rs
@@ -1,26 +1,84 @@
-// stack overflowする
-fn flip_to1(col: usize, map: &mut [u8; 30], index: usize){
-    if map[index] == 1 {
-        map[index] = 0;
+//argument
+const MaxI: usize = 5;
+const MaxJ: usize = 6;
+
+pub struct Counter {
+    pub is_first: bool,
+    pub count: usize
+}
+impl Counter {
+    pub fn new(b: bool, c: usize) -> Counter {
+        Counter{is_first: b, count: c}
+    }
+}
+
+fn flip_to1(i: usize, j: usize, map: &mut [[u8; 6]; 5], counter: &mut Counter) {
+    if map[i][j] == 1 {
+        map[i][j] = 0;
+        if counter.is_first {
+            counter.is_first = false;
+            counter.count += 1;
+        }
         // 再帰で隣接マスについてこれを呼び出す
-        flip_to1(col, map, index + 1);
-        flip_to1(col, map, index - 1);
-        flip_to1(col, map, index - col);
-        flip_to1(col, map, index + col);
+        if i == 0 {
+            if j == 0 {
+                flip_to1(i + 1, 0, map, counter);
+                flip_to1(i, 1, map, counter);
+            } else if j == MaxJ - 1 {
+                flip_to1(i + 1, j, map, counter);
+                flip_to1(i, j - 1, map, counter);
+            } else {
+                flip_to1(i + 1, j, map, counter);
+                flip_to1(i, j + 1, map, counter);
+                flip_to1(i, j - 1, map, counter);
+            }
+        } else if i == MaxI - 1 {
+            if j == 0 {
+                flip_to1(i - 1, 0, map, counter);
+                flip_to1(i, 1, map, counter);
+            } else if j == MaxJ - 1 {
+                flip_to1(i - 1, j, map, counter);
+                flip_to1(i, j - 1, map, counter);
+            } else {
+                flip_to1(i - 1, j, map, counter);
+                flip_to1(i, j + 1, map, counter);
+                flip_to1(i, j - 1, map, counter);
+            }
+        } else {
+            if j == 0 {
+                flip_to1(i + 1, 0, map, counter);
+                flip_to1(i - 1, 0, map, counter);
+                flip_to1(i, 1, map, counter);
+            } else if j == MaxJ - 1 {
+                flip_to1(i + 1, j, map, counter);
+                flip_to1(i - 1, j, map, counter);
+                flip_to1(i, j - 1, map, counter);
+            } else {
+                flip_to1(i + 1, j, map, counter);
+                flip_to1(i - 1, j, map, counter);
+                flip_to1(i, j + 1, map, counter);
+                flip_to1(i, j - 1, map, counter);
+            }
+        }
     }
 }
 
 fn main() {
-    //argument
-    let row: usize = 6;
-    let col: usize = 5;
-    let mut map: [u8; 30] = [
-        1,1,1,0,0,0,
-        1,0,0,0,1,1,
-        0,0,1,0,0,1,
-        1,1,0,0,0,0,
-        1,1,0,1,1,1];
-    for i in 0..map.len() {
-        flip_to1(col, &mut map, i);
+    let mut map: [[u8; MaxJ]; MaxI] = [
+        [1, 1, 1, 0, 0, 0],
+        [1, 0, 0, 0, 1, 1],
+        [0, 0, 1, 0, 0, 1],
+        [1, 1, 0, 0, 0, 0],
+        [1, 1, 0, 1, 1, 1],
+    ];
+    let mut counter: Counter = Counter::new(true, 0);
+    for i in 0..MaxI {
+        for j in 0..MaxJ {
+            flip_to1(i, j, &mut map, &mut counter);
+            if !counter.is_first {
+                counter.is_first = true;
+            }
+        }
     }
+    println!("the number of cluster: {}", counter.count);
 }
