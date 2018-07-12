f9f18a24b04fcc0366b474f41b2b4a5de69741fa
pj <pj.kyosyou@outlook.jp>
2018-07-13 04:08:40 +0900

    Fix: 4 to 8 in 2-2

--- src/chapter-2/2.rs
+++ src/chapter-2/2.rs
@@ -1,14 +1,17 @@
 //argument
-const MaxI: usize = 5;
-const MaxJ: usize = 6;
+const MAX_I: usize = 5;
+const MAX_J: usize = 6;
 
 pub struct Counter {
     pub is_first: bool,
-    pub count: usize
+    pub count: usize,
 }
 impl Counter {
     pub fn new(b: bool, c: usize) -> Counter {
-        Counter{is_first: b, count: c}
+        Counter {
+            is_first: b,
+            count: c,
+        }
     }
 }
 
@@ -23,57 +26,78 @@ fn flip_to1(i: usize, j: usize, map: &mut [[u8; 6]; 5], counter: &mut Counter) {
         if i == 0 {
             if j == 0 {
                 flip_to1(i + 1, 0, map, counter);
+                flip_to1(i + 1, 1, map, counter);
                 flip_to1(i, 1, map, counter);
-            } else if j == MaxJ - 1 {
+            } else if j == MAX_J - 1 {
                 flip_to1(i + 1, j, map, counter);
+                flip_to1(i + 1, j - 1, map, counter);
                 flip_to1(i, j - 1, map, counter);
             } else {
+                flip_to1(i + 1, j - 1, map, counter);
                 flip_to1(i + 1, j, map, counter);
+                flip_to1(i + 1, j + 1, map, counter);
                 flip_to1(i, j + 1, map, counter);
                 flip_to1(i, j - 1, map, counter);
             }
-        } else if i == MaxI - 1 {
+        } else if i == MAX_I - 1 {
             if j == 0 {
                 flip_to1(i - 1, 0, map, counter);
+                flip_to1(i - 1, 1, map, counter);
                 flip_to1(i, 1, map, counter);
-            } else if j == MaxJ - 1 {
+            } else if j == MAX_J - 1 {
                 flip_to1(i - 1, j, map, counter);
+                flip_to1(i - 1, j - 1, map, counter);
                 flip_to1(i, j - 1, map, counter);
             } else {
+                flip_to1(i - 1, j - 1, map, counter);
                 flip_to1(i - 1, j, map, counter);
+                flip_to1(i - 1, j + 1, map, counter);
                 flip_to1(i, j + 1, map, counter);
                 flip_to1(i, j - 1, map, counter);
             }
         } else {
             if j == 0 {
-                flip_to1(i + 1, 0, map, counter);
-                flip_to1(i - 1, 0, map, counter);
-                flip_to1(i, 1, map, counter);
-            } else if j == MaxJ - 1 {
+                flip_to1(i - 1, j, map, counter);
+                flip_to1(i - 1, j + 1, map, counter);
+                flip_to1(i, j + 1, map, counter);
                 flip_to1(i + 1, j, map, counter);
+                flip_to1(i + 1, j + 1, map, counter);
+            } else if j == MAX_J - 1 {
+                flip_to1(i - 1, j - 1, map, counter);
                 flip_to1(i - 1, j, map, counter);
                 flip_to1(i, j - 1, map, counter);
-            } else {
+                flip_to1(i + 1, j - 1, map, counter);
                 flip_to1(i + 1, j, map, counter);
+            } else {
+                flip_to1(i - 1, j - 1, map, counter);
                 flip_to1(i - 1, j, map, counter);
+                flip_to1(i - 1, j + 1, map, counter);
                 flip_to1(i, j + 1, map, counter);
                 flip_to1(i, j - 1, map, counter);
+                flip_to1(i + 1, j - 1, map, counter);
+                flip_to1(i + 1, j, map, counter);
+                flip_to1(i + 1, j + 1, map, counter);
             }
         }
     }
 }
 
 fn main() {
-    let mut map: [[u8; MaxJ]; MaxI] = [
+    let mut map: [[u8; MAX_J]; MAX_I] = [
         [1, 1, 1, 0, 0, 0],
         [1, 0, 0, 0, 1, 1],
         [0, 0, 1, 0, 0, 1],
         [1, 1, 0, 0, 0, 0],
         [1, 1, 0, 1, 1, 1],
     ];
+    println!("map = [");
+    for i in 0..MAX_I {
+        println!("    {:?}", map[i]);
+    }
+    println!("]");
     let mut counter: Counter = Counter::new(true, 0);
-    for i in 0..MaxI {
-        for j in 0..MaxJ {
+    for i in 0..MAX_I {
+        for j in 0..MAX_J {
             flip_to1(i, j, &mut map, &mut counter);
             if !counter.is_first {
                 counter.is_first = true;
